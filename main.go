ed1ffeea210e4c7de130092d465dfd011205ead9
Josias Chocooj <jchocooj@katch.com>
2017-02-16 10:40:38 -0600

    UPDATE: Handling request with http.Handler

--- main.go
+++ main.go
@@ -3,14 +3,33 @@ package main // Package where the code belongs to
 import (
 	// List of packages to be imported
 	"net/http"
+	"io/ioutil"
 )
 
 func main()  {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
+	// the second parameter asterisk means that the parameter will actually
+	// be a pointer to and http.Request object
+	http.Handle("/", new(MyHandler))
 
-	// Nil to GO that we want to use the default server multiplexer
+	// Nil to tell GO that we want to use the default server multiplexer
 	// or MUX to handle the request
 	http.ListenAndServe(":8000", nil)
+}
+
+
+type MyHandler struct {
+	http.Handler
+}
+
+func (this *MyHandler) ServeHTTP( w http.ResponseWriter, req *http.Request){
+	path := "public/"+req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+
+	if(err == nil){
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+	}
+
 }
